82fcf31c6a00750c5101c7f154ff56cdd89adca0
Sahil Manhotra <sahil0730@gmail.com>
2023-10-31 13:21:23 +0530

    if else statement in django template

--- practice/views.py
+++ practice/views.py
@@ -9,7 +9,8 @@ def homePage(request):
             { "id": 1, "name": 'test1', "email": 'test1@test.com' },
             { "id": 2, "name": 'test2', "email": 'test2@test.com' },
             { "id": 3, "name": 'test3', "email": 'test3@test.com' },
-        ]
+        ],
+        'numbers': [10,20,30,40,50]
     }
     return render(request, 'index.html', data)
 
